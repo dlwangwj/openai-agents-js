40cc5a38af82171b7d60e66e16da187ea60cd9e1
dlwangwj <dlwangwj@163.com>
2025-12-02 11:35:00 +0800

    Update next.config.ts

--- examples/realtime-next/next.config.ts
+++ examples/realtime-next/next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // 自定义字段
-  allowedDevOrigins: ["*"],
+  allowedDevOrigins: [
+    "http://localhost:3000",   // 本地访问
+    "https://localhost:3000",  // HTTPS 本地访问
+    "http://9.208.6.64:3000",  // 局域网访问
+    "https://9.208.6.64:3000"  // HTTPS 局域网访问
+  ],
 };
 
 export default nextConfig;
